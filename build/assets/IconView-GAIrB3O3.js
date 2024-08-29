b3ec71b699a940cc9338af3b259d535d65f041b0
Winar Joko Alexander <winaralex417@gmail.com>
2024-08-29 13:36:19 +0700

    fix gitignore

---
+++ build/assets/IconView-GAIrB3O3.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-D4Ddltd5.js";import{a as t}from"./index.esm-DVwSU7ju.js";import{a}from"./CRow-DMjowzGT.js";const i=s=>s.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g,"$1-$2").toLowerCase(),x=s=>Object.entries(s).map(([o,r])=>e.jsxs(a,{className:"mb-5",xs:6,sm:4,md:3,xl:2,children:[e.jsx(t,{icon:r,size:"xxl"}),e.jsx("div",{children:i(o)})]},o));export{x as g};
